035caac45b7777c66f70149bfb75c302337310a8
dmitry.smetyukhov@gmail.com <dmitry.smetyukhov@gmail.com>
2020-10-28 17:08:34 +0200

    Persons list dataSource

--- src/app/persons/persons-list/persons-list.component.ts
+++ src/app/persons/persons-list/persons-list.component.ts
@@ -1,23 +1,39 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 import {Person} from '../../shared/interfaces';
+import {Subscription} from 'rxjs';
+import {PersonDataService} from '../../shared/services/person-data.service';
 
 @Component({
   selector: 'app-persons-list',
   templateUrl: './persons-list.component.html',
   styleUrls: ['./persons-list.component.scss']
 })
-export class PersonsListComponent implements OnInit {
+export class PersonsListComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   displayedColumns: string[] = ['firstName', 'lastName', 'dateOfBirth'];
   dataSource: MatTableDataSource<Person> = new MatTableDataSource();
+  private subscriptions: Subscription[] = [];
 
-  constructor() { }
+  constructor(private personService: PersonDataService) {
+  }
 
   ngOnInit(): void {
+    this.subscriptions.push(this.personService.persons$.subscribe(list => {
+      this.dataSource.data = list;
+    }));
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(s => s.unsubscribe());
   }
 
 }
