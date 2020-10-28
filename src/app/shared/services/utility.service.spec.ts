298e91fbee434de179fe7fc198d95d0a3bf2964f
dmitry.smetyukhov@gmail.com <dmitry.smetyukhov@gmail.com>
2020-10-28 17:49:18 +0200

    Utility service calculate age spec

--- src/app/shared/services/utility.service.spec.ts
+++ src/app/shared/services/utility.service.spec.ts
@@ -1,6 +1,7 @@
 import {TestBed} from '@angular/core/testing';
 import {UtilityService} from './utility.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import * as moment from 'moment';
 
 const fakeActivatedRoute = {
   snapshot: {
@@ -26,5 +27,10 @@ describe('UtilityService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should calculate age', () => {
+    const age = service.calculateAge(moment().subtract(1, 'years').toISOString());
+    expect(age).toBe(1, 'Wrong age calculation');
   });
 });
